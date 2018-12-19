f21c04de4b5103938156cba48259eb1f8dd94809
hurikake06 <hurikake.oracle@gmail.com>
2018-12-19 05:38:07 +0000

    api追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :pets
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    scope :show do
+      get 'user/:id', to: 'raspberry#show_user'
+      get 'pet/:id', to: 'raspberry#show_pet'
+      get 'share/:id', to: 'raspberry#show_share'
+    end
+    get 'create/share/:user_id/:pet_id', to: 'raspberry#create_share'
+  end
 end
